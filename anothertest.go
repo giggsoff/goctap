c4977db5602cd5294a7406048e5e0c74b9f971a7
Fedchenkov Petr <giggsoff@gmail.com>
2018-03-14 00:44:35 +0300

    no log

--- anothertest.go
+++ anothertest.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"flag"
 	"fmt"
@@ -10,22 +11,17 @@ import (
 	"github.com/giggsoff/water"
 )
 
-
 const (
 	// I use TUN interface, so only plain IP packet, no ethernet header + mtu is set to 1300
 	BUFFERSIZE = 1500
 	MTU        = "1300"
 )
 
-
-
 var (
 	localIP  = flag.String("local", "", "Local tun interface IP/MASK like 192.168.3.3⁄24")
 	remoteIP = flag.String("remote", "", "Remote server (external) IP like 8.8.8.8")
 	port     = flag.Int("port", 4321, "UDP port for communication")
 )
-
-
 
 func runIP(args ...string) {
 	cmd := exec.Command("/sbin/ip", args...)
@@ -53,54 +49,54 @@ func main() {
 	iface, err := water.NewTUN("")
 	if nil != err {
 		log.Fatalln("Unable to allocate TUN interface:", err)
-}
-log.Println("Interface allocated:", iface.Name())
-// set interface parameters
-runIP("link", "set", "dev", iface.Name(), "mtu", MTU)
-runIP("addr", "add", *localIP, "dev", iface.Name())
-runIP("link", "set", "dev", iface.Name(), "up")
-// reslove remote addr
-remoteAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%v", *remoteIP, *port))
-if nil != err {
-log.Fatalln("Unable to resolve remote addr:", err)
-}
-// listen to local socket
-lstnAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%v", *port))
-if nil != err {
-log.Fatalln("Unable to get UDP socket:", err)
-}
-lstnConn, err := net.ListenUDP("udp", lstnAddr)
-if nil != err {
-log.Fatalln("Unable to listen on UDP socket:", err)
-}
-defer lstnConn.Close()
-// recv in separate thread
-go func() {
-	buf := make([]byte, BUFFERSIZE)
-	for {
-		n, addr, err := lstnConn.ReadFromUDP(buf)
-		// just debug
-		header, _ := ipv4.ParseHeader(buf[:n])
-		fmt.Printf("Received %d bytes from %v: %+v\n", n, addr, header)
-		if err != nil || n == 0 {
-			fmt.Println("Error: ", err)
-			continue
-		}
-		// write to TUN interface
-		iface.Write(buf[:n])
 	}
-}()
-// and one more loop
-packet := make([]byte, BUFFERSIZE)
-for {
-plen, err := iface.Read(packet)
-if err != nil {
-break
-}
-// debug :)
-header, _ := ipv4.ParseHeader(packet[:plen])
-fmt.Printf("Sending to remote: %+v (%+v)\n", header, err)
-// real send
-lstnConn.WriteToUDP(packet[:plen], remoteAddr)
-}
+	log.Println("Interface allocated:", iface.Name())
+	// set interface parameters
+	runIP("link", "set", "dev", iface.Name(), "mtu", MTU)
+	runIP("addr", "add", *localIP, "dev", iface.Name())
+	runIP("link", "set", "dev", iface.Name(), "up")
+	// reslove remote addr
+	remoteAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%v", *remoteIP, *port))
+	if nil != err {
+		log.Fatalln("Unable to resolve remote addr:", err)
+	}
+	// listen to local socket
+	lstnAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%v", *port))
+	if nil != err {
+		log.Fatalln("Unable to get UDP socket:", err)
+	}
+	lstnConn, err := net.ListenUDP("udp", lstnAddr)
+	if nil != err {
+		log.Fatalln("Unable to listen on UDP socket:", err)
+	}
+	defer lstnConn.Close()
+	// recv in separate thread
+	go func() {
+		buf := make([]byte, BUFFERSIZE)
+		for {
+			n, addr, err := lstnConn.ReadFromUDP(buf)
+			// just debug
+			//header, _ := ipv4.ParseHeader(buf[:n])
+			//fmt.Printf("Received %d bytes from %v: %+v\n", n, addr, header)
+			if err != nil || n == 0 {
+				fmt.Println("Error: ", err)
+				continue
+			}
+			// write to TUN interface
+			iface.Write(buf[:n])
+		}
+	}()
+	// and one more loop
+	packet := make([]byte, BUFFERSIZE)
+	for {
+		plen, err := iface.Read(packet)
+		if err != nil {
+			break
+		}
+		// debug :)
+		//header, _ := ipv4.ParseHeader(packet[:plen])
+		//fmt.Printf("Sending to remote: %+v (%+v)\n", header, err)
+		// real send
+		lstnConn.WriteToUDP(packet[:plen], remoteAddr)
+	}
 }
